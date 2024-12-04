10a97fa058ce5afb4f8d4701ba1860bd7ef245f1
sura-s12 <surasammour2001@gmail.com>
2024-12-05 01:28:30 +0200

    input()

---
+++ task3lec1.py
@@ -0,0 +1,5 @@
+# Task 3 solution:
+name=input("What is your name?")
+age=int(input("How old are you?"))
+years_left=100-age
+print(f'hi {name}, you have {years_left} years left until you turn 100!')
