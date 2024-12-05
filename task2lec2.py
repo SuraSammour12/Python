4205c7dfa07555fa35be1400b54b16be0a207777
sura-s12 <surasammour2001@gmail.com>
2024-12-05 02:11:46 +0200

    help()

---
+++ task2lec2.py
@@ -0,0 +1,6 @@
+# Task 2 solution : help() function
+print(help(list.append))
+print("======================================================================")
+print(help(list.pop))
+print("======================================================================")
+print(help(list.insert))
