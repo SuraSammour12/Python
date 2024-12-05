743c5dbcd382c169bafbbbb01693b7f44f795c60
sura-s12 <surasammour2001@gmail.com>
2024-12-05 15:26:56 +0200

    condition

---
+++ task4lec2.py
@@ -0,0 +1,8 @@
+# Task 4 solution: if statements
+number=int(input("Enter number : "))
+if number < 0:
+    print("Number is negative")
+elif number == 0:
+    print("Number is exactly zero")
+else:
+   print("Number is positive")
