05f2dc7914aa4c249fbd041ca71aa6930549be7f
sura-s12 <surasammour2001@gmail.com>
2024-12-06 19:38:33 +0200

    automated-list:chr()

---
+++ classroom1lec3.py
@@ -0,0 +1,10 @@
+# Classroom task solution :
+
+capital_letters=[]
+for i in range(65,91):
+    capital_letters.append(chr(i))
+print(capital_letters)
+
+print("\n")
+# chr(),ord() example
+print(ord("a")," ",chr(89))
