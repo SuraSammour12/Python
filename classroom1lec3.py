8aee66940a456ccc475d0a33b31114b627d82654
sura-s12 <surasammour2001@gmail.com>
2024-12-06 19:46:15 +0200

    english letters list

--- classroom1lec3.py
+++ classroom1lec3.py
@@ -1,10 +1,16 @@
 # Classroom task solution :
-
+# Method 1: 
 capital_letters=[]
 for i in range(65,91):
     capital_letters.append(chr(i))
 print(capital_letters)
 
 print("\n")
-# chr(),ord() example
-print(ord("a")," ",chr(89))
+
+# Method 2:
+capital_letters2=[]
+start_point=ord('A')
+end_point=ord('Z')
+for j in range(start_point,end_point+1):
+    capital_letters2.append(chr(j))
+print(capital_letters2)
