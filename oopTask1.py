51ae2b4724a081d8c2b305633619431a2c8d998e
sura-s12 <surasammour2001@gmail.com>
2024-12-12 12:25:06 +0200

    OOP:task1

--- oopTask1.py
+++ oopTask1.py
@@ -4,9 +4,11 @@ class Truck:
     def __init__(self,model,color):
         self.model=model
         self.color=color
-    
+
+model=input("Enter your model :")    
+color=input("Enter your color :")
 # Create an instance of the truck class
-my_truck=Truck("Toyota","Red")
+my_truck=Truck(model,color)
 
 # Print its model and color
 print(f"Truck model : {my_truck.model}")
