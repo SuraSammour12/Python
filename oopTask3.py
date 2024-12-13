a3ba2b5df5da593d2ddaa84fc99ca43ccf9cab2c
sura-s12 <surasammour2001@gmail.com>
2024-12-13 02:20:31 +0200

    OOP:task3

--- oopTask3.py
+++ oopTask3.py
@@ -16,9 +16,9 @@ class Student(Person):
     def more_info(self):
         print(f"Student {self.name} is {self.age} years old.")
 # Test 
-P1=Person("sara")
-P1.introduce()
-S1=Student("ammar",24)
-S1.more_info()
-S1.introduce()
+p1=Person("sara")
+p1.introduce()
+s1=Student("ammar",24)
+s1.more_info()
+s1.introduce()
 
