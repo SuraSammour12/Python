b6941ec24ff3566b483af19c243c23eced5b13fa
SuraSammour12 <surasammour2001@gmail.com>
2024-12-15 20:24:28 +0200

    Delete task3lec1.py

--- task3lec1.py
+++
@@ -1,5 +0,0 @@
-# Task 3 solution:
-name=input("What is your name?")
-age=int(input("How old are you?"))
-years_left=100-age
-print(f'hi {name}, you have {years_left} years left until you turn 100!')
