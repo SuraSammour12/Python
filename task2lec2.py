f5e45996b4ef7a6e3956fe2ffadd63cde8ffaa06
SuraSammour12 <surasammour2001@gmail.com>
2024-12-15 20:24:02 +0200

    Delete task2lec2.py

--- task2lec2.py
+++
@@ -1,6 +0,0 @@
-# Task 2 solution : help() function
-print(help(list.append))
-print("======================================================================")
-print(help(list.pop))
-print("======================================================================")
-print(help(list.insert))
