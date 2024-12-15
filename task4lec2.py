74925c568869b6fb7a6b9096e247307c641031b7
SuraSammour12 <surasammour2001@gmail.com>
2024-12-15 20:24:55 +0200

    Delete task4lec2.py

--- task4lec2.py
+++
@@ -1,8 +0,0 @@
-# Task 4 solution: if statements
-number=int(input("Enter number : "))
-if number < 0:
-    print("Number is negative")
-elif number == 0:
-    print("Number is exactly zero")
-else:
-   print("Number is positive")
